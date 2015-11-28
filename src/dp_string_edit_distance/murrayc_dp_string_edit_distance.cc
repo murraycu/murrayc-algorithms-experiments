8676782c8a7105538b7372df44f82f7381c045b4
Murray Cumming <murrayc@murrayc.com>
2015-11-28 15:13:39 +0100

    Rename m to costs

--- src/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
+++ src/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
@@ -28,25 +28,25 @@ uint dp_calc_edit_distance(const std::string& str, const std::string& pattern) {
   const auto str_size = str.size();
   const auto pattern_size = pattern.size();
   
-  type_costs m(str_size);
-  for(auto& sub : m) {
+  type_costs costs(str_size);
+  for(auto& sub : costs) {
     sub.resize(pattern_size, 0);
   }
 
   for (uint i = 1; i < str_size; ++i) {
     for (uint j = 1; j < pattern_size; ++j) {
       //Get the cost of the possible operations, and choose the least costly:
-      const uint cost_match = m[i-1][j-1] + match(str[i], pattern[j]);
-      const uint cost_insert = m[i][j-1] + indel(pattern[j]);
-      const uint cost_delete = m[i-1][j] + indel(str[j]);
+      const uint cost_match = costs[i-1][j-1] + match(str[i], pattern[j]);
+      const uint cost_insert = costs[i][j-1] + indel(pattern[j]);
+      const uint cost_delete = costs[i-1][j] + indel(str[j]);
       
       auto min = std::min(cost_match, cost_insert);
       min = std::min(min, cost_delete);
-      m[i][j] = min;
+      costs[i][j] = min;
     }
   }
   
-  return m[str_size-1][pattern_size-1];
+  return costs[str_size-1][pattern_size-1];
 }
 
 int main() {
