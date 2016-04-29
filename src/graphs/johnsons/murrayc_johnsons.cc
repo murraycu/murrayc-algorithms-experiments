cd36704823c1b1177748376c922e93b98bcd46aa
Murray Cumming <murrayc@murrayc.com>
2016-04-29 14:57:23 +0200

    simplify.

--- src/graphs/johnsons/murrayc_johnsons.cc
+++ src/graphs/johnsons/murrayc_johnsons.cc
@@ -24,8 +24,6 @@ type_length johnsons_all_pairs_shortest_path(const type_vec_nodes& vertices_in, 
   if(vertices.empty())
     return Edge::LENGTH_INFINITY;
 
-  //const type_num vertices_count = vertices.size();
-
   //Add an extra vertex s, with zero-weight paths to every existing vertex:
   //There will be no paths into vertex s, so it will not disturb any
   //shortest paths calculations from any other vertex.
@@ -35,8 +33,8 @@ type_length johnsons_all_pairs_shortest_path(const type_vec_nodes& vertices_in, 
   const type_num s = size - 1;
   auto& vertex_s = vertices_with_s[s];
   for (type_num i = 0; i < size; ++i) {
-    //Add a zero-weight edge from our extra S vertex to the other vertex:
-    vertex_s.edges_.emplace_back(i, 0); 
+    //Add a zero-weight edge from our extra s vertex to the other vertex:
+    vertex_s.edges_.emplace_back(i, 0);
   }
 
   //Run Bellman-Ford single source shortest path algorithm from our new vertex s to 
@@ -48,33 +46,24 @@ type_length johnsons_all_pairs_shortest_path(const type_vec_nodes& vertices_in, 
     std::cerr << "Negative cycle found." << std::endl;
     return Edge::LENGTH_INFINITY;
   }
-  
-  //The shortest path from the extra vertex s in the reweighting stage
-  //of Johnson's APSP algorithm:
-  std::unordered_map<type_num, type_length> map_vertex_length_johnsons_sp;
 
-  for (type_num i = 0; i < size; ++i) { //Not including the new vertex_s.
-    const auto path_length = shortest_paths[i].length_;
-    //std:: cout << "s=" << s << ", i=" << i << ", length_johnsons_sp_: " << path_length << std::endl;
-    map_vertex_length_johnsons_sp[i] = path_length;
-  }
-  
-  //Change the edge lengths based on the discovered vertex values
+  //Change the edge lengths based on the discovered values for each vertex
   //(single source shortest paths from our new vertex s):
   //Now they will all be positive.
   for (type_num i = 0; i < size; ++i) { //Not including the new vertex_s.
-    const auto vertex_length_johnsons_sp = map_vertex_length_johnsons_sp[i];
+    const auto shortest_path_for_i = shortest_paths[i].length_;
     
     auto& vertex = vertices[i];
     for (auto& edge : vertex.edges_) {
-      const auto dest_vertex_length_johnsons_sp = map_vertex_length_johnsons_sp[edge.destination_vertex_];
+      const auto shortest_path_for_dest = shortest_paths[edge.destination_vertex_].length_;
 
-      //std::cout << "original edge length: " << edge.length_ << std::endl;
-      //std:: cout << "  source=" << i << ", dest=" << edge.destination_vertex_ << std::endl;
-      //std:: cout << "  vertex.length_johnsons_sp_=" << vertex.length_johnsons_sp_ << ", vertex_dest.length_johnsons_sp_=" <<  vertex_dest.length_johnsons_sp_ << std::endl;
+      std::cout << "original edge length: " << edge.length_ << std::endl;
+      std:: cout << "  source=" << i << ", dest=" << edge.destination_vertex_ << std::endl;
+      std:: cout << "  shortest_path_for_i =" << shortest_path_for_i <<
+        ", vertex_dest.shortest_path_for_dest=" <<  shortest_path_for_dest << std::endl;
       edge.length_ +=
-        (vertex_length_johnsons_sp - dest_vertex_length_johnsons_sp);
-      //std::cout << "new edge length: " << edge.length_ << std::endl;
+        (shortest_path_for_i - shortest_path_for_dest);
+      std::cout << "new edge length: " << edge.length_ << std::endl;
       if (edge.length_ < 0) {
         std::cerr << "Error: The new edge length should not be negative: " << edge.length_ << std::endl;
         return Edge::LENGTH_INFINITY;
@@ -86,17 +75,20 @@ type_length johnsons_all_pairs_shortest_path(const type_vec_nodes& vertices_in, 
   //the reweighted graph:
   type_length min = Edge::LENGTH_INFINITY;
   for (type_num u = 0; u < size; ++u) {
+     const auto shortest_path_u = shortest_paths[u].length_;
     for (type_num v = 0; v < size; ++v) {
       if (u == v)
         continue;
+        
+      const auto shortest_path_v = shortest_paths[v].length_;
 
       std::cout << "u=" << u << ", v=" << v << std::endl;
       const auto shortest_path = dijkstra_compute_shortest_path(vertices_with_s, u, v);
-      
+
       //Convert to the length of the path in the original set of vertices
       //(with negative edge weights):
       const type_length real_shortest_path = shortest_path.length_
-        - map_vertex_length_johnsons_sp[u] + map_vertex_length_johnsons_sp[v];
+        - shortest_path_u + shortest_path_v;
       if (real_shortest_path < min) {
         min = real_shortest_path;
       }
