24cb8ac643193d79bcb6a1b9c15e12a35ff5a460
Murray Cumming <murrayc@murrayc.com>
2016-05-15 09:35:05 +0200

    BFS: Rename test function.

--- src/graphs/breadth_first_search/murrayc_breadth_first_search.cc
+++ src/graphs/breadth_first_search/murrayc_breadth_first_search.cc
@@ -5,7 +5,7 @@
 #include <cassert>
 
 static
-void test_small_dest(type_num source_vertex_num, type_num dest_vertex_num, const std::vector<type_num>& expected_path)
+void test_small(type_num source_vertex_num, type_num dest_vertex_num, const std::vector<type_num>& expected_path)
 {
   std::vector<type_num> path_vertices;
 
@@ -29,21 +29,21 @@ void test_small_dest(type_num source_vertex_num, type_num dest_vertex_num, const
 int main()
 {
 /*
-  test_small_dest(0, 0, {});
-  test_small_dest(0, 1, {SourceAndEdge(0, 0)}); //0 to 1, using the first edge in 0.
-  test_small_dest(1, 3, {SourceAndEdge(1, 1)}); //1 to 3, using the second edge in 1.
-  test_small_dest(2, 3, {SourceAndEdge(2, 0)}); //2 to 3, using the first edge in 1.
-  test_small_dest(0, 3, {
+  test_small(0, 0, {});
+  test_small(0, 1, {SourceAndEdge(0, 0)}); //0 to 1, using the first edge in 0.
+  test_small(1, 3, {SourceAndEdge(1, 1)}); //1 to 3, using the second edge in 1.
+  test_small(2, 3, {SourceAndEdge(2, 0)}); //2 to 3, using the first edge in 1.
+  test_small(0, 3, {
     SourceAndEdge(0, 0), //0 to 1, using the first edge in 0.
     SourceAndEdge(1, 1) //1 to 3, using the second edge in 1.
   });
 */
 
-  test_small_dest(0, 0, {0});
-  test_small_dest(0, 1, {0, 1});
-  test_small_dest(1, 3, {1, 3});
-  test_small_dest(2, 3, {2, 3});
-  test_small_dest(0, 3, {0, 1, 3});
+  test_small(0, 0, {0});
+  test_small(0, 1, {0, 1});
+  test_small(1, 3, {1, 3});
+  test_small(2, 3, {2, 3});
+  test_small(0, 3, {0, 1, 3});
 
   return EXIT_SUCCESS;
 }
