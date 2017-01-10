0580a9a3947b3d922fae405e294817448698029e
Murray Cumming <murrayc@murrayc.com>
2017-01-10 07:52:35 +0100

    Rabin-Karp: Slight reorganization.

--- src/substring_search/rabin_karp/main.cc
+++ src/substring_search/rabin_karp/main.cc
@@ -4,6 +4,21 @@
 #include <string>
 #include <vector>
 
+static inline uint32_t
+hash(const std::string& str, std::size_t len, uint32_t R, uint32_t Q) {
+  uint32_t h = 0;
+  for (auto i = 0u; i < len; ++i) {
+    h = (h * R + str[i]) % Q;
+  }
+
+  return h;
+}
+
+static inline uint32_t
+hash(const std::string& str, uint32_t R, uint32_t Q) {
+  return hash(str, str.size(), R, Q);
+}
+
 static std::vector<std::size_t>
 find(const std::string& str, const std::string& pat) {
   std::vector<std::size_t> result;
@@ -17,20 +32,17 @@ find(const std::string& str, const std::string& pat) {
 
   constexpr uint32_t R = 256; // Alphabet size.
   constexpr uint32_t Q = 937; // A large prime.
-  uint32_t RM = 1;
-  for (auto i = 1u; i < m; ++i) {
-    RM = (RM * R) % Q;
-  }
 
-  uint32_t pHash = 0;
-  uint32_t sHash = 0;
-  for (auto i = 0u; i < m; ++i) {
-    pHash = (pHash * R + pat[i]) % Q;
-    sHash = (sHash * R + str[i]) % Q;
-  }
+  const auto pHash = hash(pat, R, Q);
+  auto sHash = hash(str, m, R, Q);
 
   if (pHash == sHash) {
     result.emplace_back(0);
+  }
+
+  uint32_t RM = 1;
+  for (auto i = 1u; i < m; ++i) {
+    RM = (RM * R) % Q;
   }
 
   for (auto i = m; i < n; ++i) {
