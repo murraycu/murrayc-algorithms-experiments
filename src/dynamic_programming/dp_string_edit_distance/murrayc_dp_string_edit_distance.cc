051b54040f7c4f23d1048512f3c8a45befd3e3d4
Murray Cumming <murrayc@murrayc.com>
2017-03-13 13:02:53 +0100

    DP: Edit Distance: Corrections.
    Properly deal with the i=0 or j=0 base cases,
    which should mean converting from or to an empty string.

--- src/dynamic_programming/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
+++ src/dynamic_programming/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -27,20 +28,39 @@ dp_calc_edit_distance(const std::string& str, const std::string& pattern) {
   const auto str_size = str.size();
   const auto pattern_size = pattern.size();
 
+  // 0 means empty string.
+  // and 1 is just the first character of the string.
+
   // We switch between a and b:
-  type_costs a(pattern_size, 0);
-  type_costs b(pattern_size, 0);
+  type_costs a(pattern_size + 1, 0);
+  type_costs b(pattern_size + 1, 0);
   bool a_is_first = false; // arbitrary.
 
-  for (uint i = 1; i < str_size; ++i) {
+  for (uint i = 0; i <= str_size; ++i) {
     a_is_first = !a_is_first;
     type_costs& costs_i = (a_is_first ? a : b);
     const type_costs& costs_i_minus_1 = (a_is_first ? b : a);
 
-    for (uint j = 1; j < pattern_size; ++j) {
+    for (uint j = 0; j <= pattern_size; ++j) {
+      // 0 means empty string, so converting an empty to non-empty, or vice-versa,
+      // means adding or deleting all the characters
+      if (i == 0) {
+        // Add all the characters from B:
+        costs_i[j] = j;
+        continue;
+      } else if (j == 0) {
+        // Delete all the characters from A:
+        costs_i[j] = i;
+        continue;
+      }
+
       // Get the cost of the possible operations, and choose the least costly:
+
+      // We subtract 1 when comparing characters, because i and j are 1-indexed,
+      // with 0 meaning an empty string.
       const uint cost_match =
-        costs_i_minus_1[j - 1] + match(str[i], pattern[j]);
+        costs_i_minus_1[j - 1] + match(str[i - 1], pattern[j - 1]);
+
       const uint cost_insert = costs_i[j - 1] + indel(pattern[j]);
       const uint cost_delete = costs_i_minus_1[j] + indel(str[j]);
 
@@ -53,8 +73,8 @@ dp_calc_edit_distance(const std::string& str, const std::string& pattern) {
     // and costs_i_minus_1 will be filled as costs_i;
   }
 
-  type_costs& costs_i = (a_is_first ? a : b);
-  return costs_i[pattern_size - 1];
+  const type_costs& costs_i = (a_is_first ? a : b);
+  return costs_i[pattern_size];
 }
 
 int
@@ -66,6 +86,16 @@ main() {
   std::cout << "string: " << str << std::endl
             << "pattern: " << pattern << std::endl
             << "distance: " << distance << std::endl;
+
+  assert(dp_calc_edit_distance("you should not", "though shalt not") == 7);
+
+  assert(dp_calc_edit_distance("", "") == 0);
+  assert(dp_calc_edit_distance("a", "") == 1);
+  assert(dp_calc_edit_distance("", "a") == 1);
+  assert(dp_calc_edit_distance("a", "b") == 1);
+  assert(dp_calc_edit_distance("aaa", "aa") == 1);
+  assert(dp_calc_edit_distance("aa", "aaa") == 1);
+  assert(dp_calc_edit_distance("abac", "aac") == 1);
 
   return EXIT_SUCCESS;
 }
